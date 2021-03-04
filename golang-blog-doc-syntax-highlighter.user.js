6e7d51a0fa4d7dd65c81307aa0a5313934cf0bc2
ttys3 <ttys3@outlook.com>
2021-03-04 21:48:40 +0800

    bump to v0.1.4

--- golang-blog-doc-syntax-highlighter.user.js
+++ golang-blog-doc-syntax-highlighter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Golang Blog Doc Syntax Highlighter
 // @namespace    https://ttys3.dev
-// @version      v0.1.3
+// @version      v0.1.4
 // @description  add Syntax highlighter for https://blog.golang.org
 // @author       荒野無燈
 // @updateURL    https://cdn.jsdelivr.net/gh/ttys3/userjs@latest/golang-blog-doc-syntax-highlighter.user.js
 // @match        https://blog.golang.org/*
 // @match        https://golang.org/*
 // @match        https://pkg.go.dev/*
-// @require      https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.3/static/js/prismjs/blog.golang.org/prism.js
-// @resource     prismcss  https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.3/static/js/prismjs/blog.golang.org/prism.css
+// @require      https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.4/static/js/prismjs/blog.golang.org/prism.js
+// @resource     prismcss  https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.4/static/js/prismjs/blog.golang.org/prism.css
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 
@@ -26,6 +26,12 @@
         document.querySelectorAll('pre code').
             forEach(function(e) {
                 // ES6 spread operator
+                e.classList.add(...classesToAdd);
+            });
+
+        // https://golang.org/src/*  : pre
+        document.querySelectorAll('div.container pre').
+            forEach(function(e) {
                 e.classList.add(...classesToAdd);
             });
     }
