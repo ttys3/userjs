a83f64cd7878ceb926d6d695b86633423d74cebc
ttys3 <ttys3@outlook.com>
2021-04-21 15:56:43 +0800

    style: format code with deno lsp

--- packtpub-syntax-highlighter.user.js
+++ packtpub-syntax-highlighter.user.js
@@ -14,29 +14,26 @@
 
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    GM_addStyle(GM_getResourceText('prismcss'));
+(function () {
+  "use strict";
+  GM_addStyle(GM_getResourceText("prismcss"));
 
+  const doHightLight = (e) => {
+    if (!e.classList) {
+      return;
+    }
+    if (!e.classList.contains("language-markup") || e.tagName != "CODE") {
+      return;
+    }
 
+    if (e.classList.contains("rainbow-braces")) {
+      return;
+    }
 
+    let classesToAdd = ["rainbow-braces", "line-numbers"];
+    classesToAdd.push("language-rust");
 
-    const doHightLight = (e) => {
-        if (!e.classList) {
-            return;
-        }
-        if (!e.classList.contains('language-markup') || e.tagName != 'CODE') {
-            return;
-        }
-
-        if (e.classList.contains('rainbow-braces')) {
-            return;
-        }
-
-    let classesToAdd = ['rainbow-braces', 'line-numbers'];
-        classesToAdd.push('language-rust');
-
-        /*
+    /*
     const bookName = top.document.querySelector('.book-title').innerText.toLowerCase();
     if (bookName.includes('rust')) {
         classesToAdd.push('language-rust');
@@ -45,79 +42,78 @@
     }
     */
 
-                // ES6 spread operator
-                e.classList.add(...classesToAdd);
-        // Returns a highlighted HTML string
-const html = Prism.highlight(e.innerText, Prism.languages.rust, 'rust');
-                e.innerHTML = html;
-            console.log(e.innerText);
-            e.classList.remove("language-markup");
+    // ES6 spread operator
+    e.classList.add(...classesToAdd);
+    // Returns a highlighted HTML string
+    const html = Prism.highlight(e.innerText, Prism.languages.rust, "rust");
+    e.innerHTML = html;
+    console.log(e.innerText);
+    e.classList.remove("language-markup");
+  };
 
-            };
-
-    const handleEvent = (e) => {
-
+  const handleEvent = (e) => {
     if (/beta\.packtpub\.com/.test(location.hostname)) {
-        console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-        document.querySelectorAll('pre code').
-            forEach(() => {});
+      console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+      document.querySelectorAll("pre code")
+        .forEach(() => {});
     }
-    };
+  };
 
-    setTimeout( () => {
+  setTimeout(() => {
+    const s = document.createElement("script");
+    s.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.5/static/js/prismjs/packtpub.com/prism.js",
+    );
+    document.body.appendChild(s);
 
-  const s = document.createElement( 'script' );
-  s.setAttribute( 'src', 'https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.5/static/js/prismjs/packtpub.com/prism.js' );
-  document.body.appendChild( s );
+    const linkElement = document.createElement("link");
+    linkElement.setAttribute("rel", "stylesheet");
+    linkElement.setAttribute("type", "text/css");
+    linkElement.setAttribute(
+      "href",
+      "https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.5/static/js/prismjs/packtpub.com/prism.css",
+    );
+    document.body.appendChild(linkElement);
 
-
-const linkElement = document.createElement('link');
-linkElement.setAttribute('rel', 'stylesheet');
-linkElement.setAttribute('type', 'text/css');
-linkElement.setAttribute('href', 'https://cdn.jsdelivr.net/gh/ttys3/userjs@v0.1.5/static/js/prismjs/packtpub.com/prism.css');
-document.body.appendChild( linkElement );
-
-const fixupCodeStyle = document.createElement('style')
-fixupCodeStyle.innerText = `pre[class*=language-]>code {
+    const fixupCodeStyle = document.createElement("style");
+    fixupCodeStyle.innerText = `pre[class*=language-]>code {
     border-top: 16px solid transparent !important;
     background-color: transparent !important;
     background-image: none !important;
-}`
-document.head.appendChild(fixupCodeStyle)
+}`;
+    document.head.appendChild(fixupCodeStyle);
 
-        document.addEventListener("click", (e) => {
-            doHightLight(e.target);
-        });
+    document.addEventListener("click", (e) => {
+      doHightLight(e.target);
+    });
 
-        // Select the node that will be observed for mutations
-const targetNode = document.querySelector( 'body' );
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelector("body");
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
 
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for(const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            //console.log('A child node has been added or removed. %o', mutation.target);
-            doHightLight(mutation.target);
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+      // Use traditional 'for loops' for IE 11
+      for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          //console.log('A child node has been added or removed. %o', mutation.target);
+          doHightLight(mutation.target);
+        } else if (mutation.type === "attributes") {
+          //console.log('The ' + mutation.attributeName + ' attribute was modified.');
         }
-        else if (mutation.type === 'attributes') {
-            //console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
-    }
-};
+      }
+    };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
 
-// Later, you can stop observing
-// observer.disconnect();
-    }, 1000);
-
+    // Later, you can stop observing
+    // observer.disconnect();
+  }, 1000);
 })();
-
